c38830ddd26530618bc9b1bef949e1e478c332bd
João Victor <joao.adm.etec@outlook.com>
2023-11-23 00:00:40 -0300

    Criação dos serviços relacionados ao estágio.

---
+++ Unipsico/Services/EstagioServices.cs
@@ -0,0 +1,68 @@
+﻿using Unipsico.Data;
+using Unipsico.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Unipsico.Services
+{
+    public class EstagioServices
+    {
+        private readonly ApplicationDB _applicationDB;
+
+        public EstagioServices(ApplicationDB applicationDB)
+        {
+            _applicationDB = applicationDB;
+        }
+
+        public async Task<IEnumerable<EstagioModel>> ObterTodosEstagios()
+        {
+            return await _applicationDB.Estagios
+                .Include(e => e.InstituicaoCredenciada)
+                .Include(e => e.Aluno)
+                .ToListAsync();
+        }
+
+        public async Task<EstagioModel> ObterEstagioPorId(Guid id)
+        {
+             var estagio = await _applicationDB.Estagios
+                .Include(e => e.InstituicaoCredenciada)
+                .Include(e => e.Aluno)
+                .FirstOrDefaultAsync(e => e.EstagioId == id);
+
+            if (estagio == null)
+            {
+                throw new InvalidOperationException("Nenhum estágio encontrado!");
+            }
+
+            return estagio;
+        }
+
+        public async Task AdicionarEstagio(EstagioModel estagio)
+        {
+            _applicationDB.Estagios.Add(estagio);
+            await _applicationDB.SaveChangesAsync();
+        }
+
+        public async Task AtualizarEstagio(EstagioModel estagio)
+        {
+            var estagioNoBancoDeDados = await _applicationDB.Estagios
+                .FirstOrDefaultAsync(e => e.EstagioId == estagio.EstagioId);
+
+            if (estagioNoBancoDeDados != null)
+            {
+                _applicationDB.Entry(estagioNoBancoDeDados).CurrentValues.SetValues(estagio);
+                await _applicationDB.SaveChangesAsync();
+            }
+        }
+
+        public async Task ExcluirEstagio(Guid id)
+        {
+            var estagio = await _applicationDB.Estagios.FindAsync(id);
+
+            if (estagio != null)
+            {
+                _applicationDB.Estagios.Remove(estagio);
+                await _applicationDB.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ Unipsico/Services/Interfaces/IEstagioServices.cs
@@ -0,0 +1,13 @@
+﻿using Unipsico.Models;
+
+namespace Unipsico.Services.Interfaces
+{
+    public interface IEstagioServices
+    {
+        Task<IEnumerable<EstagioModel>> ObterTodosEstagios();
+        Task<EstagioModel> ObterEstagioPorId(Guid id);
+        Task AdicionarEstagio(EstagioModel estagio);
+        Task AtualizarEstagio(EstagioModel estagio);
+        Task ExcluirEstagio(Guid id);
+    }
+}
